df21f6fbd11d89ef2e66bb1759a2c3a23078422e
Yi Yang @ Alke <ahyangyi@gmail.com>
2022-02-11 19:33:11 +0800

    Random (literally!) examples

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use dominion_simulator::game;
 use dominion_simulator::kingdom;
+use rand::Rng;
 
 fn main() {
     let a: game::Game<kingdom::SimpleKingdom> = game::Game::make(2);
-    println!("Hello, world!");
+
+    let mut x: u32 = 0;
+    for i in 0..100000000 {
+        x += rand::thread_rng().gen_range(1..101);
+    }
+    println!("{}", x);
 }
