46c1ad119a6189e710d3ef1120f0194e1bcd9eeb
Yi Yang @ Alke <ahyangyi@gmail.com>
2022-02-14 17:43:01 +0800

    Build (unused) big money controller

--- src/controller/big_money.rs
+++ src/controller/big_money.rs
@@ -7,9 +7,18 @@
 //   Buy Duchy if remaining province <= 6
 //   Buy silver
 use crate::kingdom;
+use crate::game;
+use crate::controller;
+use std::marker::PhantomData;
 
-trait Controller<K: Kingdom> {
-    fn buy(&mut self, game: mut &Game, hand: mut &PersonalState) {
-        game.buy_gold() || game.buy_silver();
+pub struct BigMoneyController<K: kingdom::Kingdom> {
+    kingdom: PhantomData<K>,
+}
+
+impl<K: kingdom::Kingdom> controller::Controller<K> for BigMoneyController<K> {
+    fn act(&mut self) {
+    }
+    fn buy(&mut self, game: &mut game::Game<K, 2>, hand: &mut game::PersonalState) {
+        // game.buy_gold() || game.buy_silver();
     }
 }
--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,6 +1,8 @@
 use crate::kingdom;
 use crate::game;
 
+mod big_money;
+
 pub trait Controller<K: kingdom::Kingdom> {
     fn act(&mut self);
     fn buy(&mut self, game: &mut game::Game<K, 2>, hand: &mut game::PersonalState);
