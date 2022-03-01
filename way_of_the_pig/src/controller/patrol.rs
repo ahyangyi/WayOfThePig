50164e029c13039fbe65ff4d45515633f7b9fbb0
Yi Yang @ Anteros <ahyangyi@gmail.com>
2022-03-02 00:56:31 +0800

    Adapt the Patrol strategy for colony games

--- way_of_the_pig/src/controller/patrol.rs
+++ way_of_the_pig/src/controller/patrol.rs
@@ -29,23 +29,48 @@ impl controller::Controller for Controller {
         game.play_patrol::<P>();
     }
     fn buy<G: game::GameState, const P: usize>(&mut self, game: &mut G) {
+        while game.play_platinum::<P>() {}
         while game.play_gold::<P>() {}
         while game.play_silver::<P>() {}
         while game.play_copper::<P>() {}
-        if total_money::<G, P>(game) > 15 && game.buy_province::<P>() {
-            return;
-        } else if game.province_in_supply() <= 4 && game.buy_duchy::<P>() {
-            return;
-        } else if game.province_in_supply() <= 2 && game.buy_estate::<P>() {
-            return;
-        } else if game.buy_gold::<P>() {
-            return;
-        } else if game.province_in_supply() <= 6 && game.buy_duchy::<P>() {
-            return;
-        } else if game.get_player::<P>().count_card(card::CardType::Patrol) * 8 < num_money::<G, P>(game) && game.buy_patrol::<P>() {
-            return;
+        if game.colony_enabled() {
+            if game.buy_colony::<P>() {
+                return;
+            } else if game.colony_in_supply() <= 6 && game.buy_province::<P>() {
+                return;
+            } else if game.colony_in_supply() <= 5 && game.buy_duchy::<P>() {
+                return;
+            } else if game.colony_in_supply() <= 2 && game.buy_estate::<P>() {
+                return;
+            } else if game.buy_platinum::<P>() {
+                return;
+            } else if game.colony_in_supply() <= 7 && game.buy_province::<P>() {
+                return;
+            } else if game.buy_gold::<P>() {
+                return;
+            } else if game.colony_in_supply() <= 6 && game.buy_duchy::<P>() {
+                return;
+            } else if game.get_player::<P>().count_card(card::CardType::Patrol) * 8 < num_money::<G, P>(game) && game.buy_patrol::<P>() {
+                return;
+            } else {
+                game.buy_silver::<P>();
+            }
         } else {
-            game.buy_silver::<P>();
+            if total_money::<G, P>(game) > 15 && game.buy_province::<P>() {
+                return;
+            } else if game.province_in_supply() <= 4 && game.buy_duchy::<P>() {
+                return;
+            } else if game.province_in_supply() <= 2 && game.buy_estate::<P>() {
+                return;
+            } else if game.buy_gold::<P>() {
+                return;
+            } else if game.province_in_supply() <= 6 && game.buy_duchy::<P>() {
+                return;
+            } else if game.get_player::<P>().count_card(card::CardType::Patrol) * 8 < num_money::<G, P>(game) && game.buy_patrol::<P>() {
+                return;
+            } else {
+                game.buy_silver::<P>();
+            }
         }
     }
 }
