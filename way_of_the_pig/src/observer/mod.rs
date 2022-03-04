e8b288df3cf2f586ad5284b86351df6bdc741751
Yi Yang @ Anteros <ahyangyi@gmail.com>
2022-03-04 12:46:57 +0800

    ..

--- way_of_the_pig/src/observer/mod.rs
+++ way_of_the_pig/src/observer/mod.rs
@@ -4,4 +4,5 @@ pub mod round_stats;
 pub trait Observer {
     fn notify_result_2(&mut self, _result: [u8; 2]) {}
     fn notify_round(&mut self, _round: u32) {}
+    fn add_coin<const P: usize>(&mut self, _c: u32) {}
 }
--- way_of_the_pig/src/observer/round_stats.rs
+++ way_of_the_pig/src/observer/round_stats.rs
@@ -4,15 +4,15 @@ const MAX_ROUND: usize = 100;
 
 #[derive(Copy, Clone)]
 pub struct RoundStats {
-    coin: [u32; MAX_ROUND],
-    vp: [u32; MAX_ROUND],
-    round: [u32; MAX_ROUND],
+    coin: [[u32; MAX_ROUND]; 2],
+    vp: [[u32; MAX_ROUND]; 2],
+    round: [[u32; MAX_ROUND]; 2],
 }
 
 impl observer::Observer for RoundStats {
     fn notify_round(&mut self, round: u32) {
         if round < MAX_ROUND as u32 {
-            self.round[round as usize] += 1;
+            self.round[0][round as usize] += 1;
         }
     }
 }
