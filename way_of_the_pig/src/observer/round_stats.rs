cb109460ca9a2a222a73415f32aa109c2f8c289f
Yi Yang @ Anteros <ahyangyi@gmail.com>
2022-03-05 00:26:55 +0800

    coin stats

--- way_of_the_pig/src/observer/round_stats.rs
+++ way_of_the_pig/src/observer/round_stats.rs
@@ -7,12 +7,19 @@ pub struct RoundStats {
     coin: [[u32; MAX_ROUND]; 2],
     vp: [[u32; MAX_ROUND]; 2],
     round: [[u32; MAX_ROUND]; 2],
+    current_round: u32,
 }
 
 impl observer::Observer for RoundStats {
-    fn notify_round(&mut self, round: u32) {
+    fn notify_turn<const P: usize>(&mut self, round: u32) {
+        self.current_round = round;
         if round < MAX_ROUND as u32 {
-            self.round[0][round as usize] += 1;
+            self.round[P][round as usize] += 1;
+        }
+    }
+    fn add_coin<const P: usize>(&mut self, c: u32) {
+        if self.current_round < MAX_ROUND as u32 {
+            self.coin[P][self.current_round as usize] += c;
         }
     }
 }
