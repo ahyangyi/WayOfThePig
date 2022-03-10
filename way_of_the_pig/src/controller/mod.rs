26901d3ba58bdb3c890ee098b45b398dbc9c3880
Yi Yang @ Alke <ahyangyi@gmail.com>
2022-03-10 18:06:20 +0800

    Revert to WDL observer; add TFB interface

--- way_of_the_pig/src/controller/mod.rs
+++ way_of_the_pig/src/controller/mod.rs
@@ -1,3 +1,4 @@
+use crate::card::CardType;
 use crate::game;
 
 pub mod big_money;
@@ -9,4 +10,8 @@ pub mod smithy_accidental_village;
 pub trait Controller {
     fn act<G: game::GameState, const P: usize>(&mut self, _game: &mut G) {}
     fn buy<G: game::GameState, const P: usize>(&mut self, _game: &mut G) {}
+    // FIXME None isn't even legal, but we don't have a sensible default currently
+    fn trash_for_upgrade<G: game::GameState, const P: usize>(&mut self, _game: &mut G) -> Option<CardType> {
+        None
+    }
 }
