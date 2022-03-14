42afb9c003450608a36c8ba19a2aa093935ceba3
Yi Yang @ Aura-Catalina <ahyangyi@gmail.com>
2022-03-14 16:00:49 +0800

    Generic programming (doesn't work yet)

--- way_of_the_pig/src/game/run_round.rs
+++ way_of_the_pig/src/game/run_round.rs
@@ -1,11 +1,19 @@
 use crate::controller;
 use crate::game::GameState;
 use crate::observer::Observer;
+use frunk::*;
 
 pub trait RoundPlayer {
     fn run_round<G>(&mut self, game: &mut G, round: u32) -> i8
     where
         G: GameState;
+}
+
+trait TurnPlayer {
+    fn run_turn<G, I>(&mut self, game: &mut G, round: u32) -> i8
+    where
+        G: GameState,
+        I: hlist::HList;
 }
 
 impl<T1, T2> RoundPlayer for (&mut T1, &mut T2)
@@ -35,5 +43,38 @@ where
         }
         game.get_player::<1>().clean_up();
         -1
+    }
+}
+
+impl TurnPlayer for HNil {
+    #[inline]
+    fn run_turn<G, I>(&mut self, game: &mut G, round: u32) -> i8
+    where
+        G: GameState,
+        I: hlist::HList,
+    {
+        -1
+    }
+}
+
+impl<H, T> TurnPlayer for HCons<&mut H, T>
+where
+    H: controller::Controller,
+{
+    #[inline]
+    fn run_turn<G, I>(&mut self, game: &mut G, round: u32) -> i8
+    where
+        G: GameState,
+        I: hlist::HList,
+    {
+        game.get_observer().notify_turn::<{ I::LEN }>(round);
+        game.get_player::<I::LEN>().turn_start();
+        self.head.act::<G, I::LEN>(game);
+        self.head.buy::<G, I::LEN>(game);
+        if game.end() {
+            return I::LEN;
+        }
+        game.get_player::<I::LEN>().clean_up();
+        self.tail.run_turn::<G, HCons<H, I>>(game, round)
     }
 }
